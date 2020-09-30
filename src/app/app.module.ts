eee2303e85536c4877ac3dc933e2852aef1432f2
= <=>
2020-09-30 11:39:07 +0200

    nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+import { AuthModule } from './auth/auth.module';
 import { PagesModule } from './pages/pages.module';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -6,9 +9,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+
+
 
 
 
@@ -16,15 +18,15 @@ import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component'
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
+   
     NopagefoundComponent,
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
